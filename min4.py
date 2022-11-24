12c237d61cd17faedeeed0680c726ee57e799aed
Arnold <leeice@mail.ru>
2022-11-24 19:21:38 +0300

    dzzzzz

--- min4.py
+++ min4.py
@@ -1,77 +1,64 @@
-#Вычислить число c заданной точностью d
-from math import pi
+#задача 1 -----------------------------------------------------------------------------
 
-d = int(input("Введите число для заданной точности числа Пи:\n"))
-print(f'число Пи с заданной точностью {d} равно {round(pi, d)}')
+list1 = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
 
-
-
-#Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
-num = int(input("Введите число: "))
-i = 2 # первое простое число
-lst = []
-old = num
-while i <= num:
-    if num % i == 0:
-        lst.append(i)
-        num //= i
-        i = 2
+lenght: int = len(list1)
+store_id = id(list1)
+for i in range(lenght):
+    entity = list1.pop(0)
+    if  entity.isdigit():
+        list1.append(F'"{int(entity):02d}"')
+    elif entity[0] == "+" and entity[1].isdigit():
+        list1.append(F'"+{int(entity):02d}"')
     else:
-        i += 1
-print(f"Простые множители числа {old} приведены в списке: {lst}")
+        list1.append(entity)
+print(' '.join(list1))
 
 
-#Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
-
-lst = list(map(int, input("Введите числа через пробел:\n").split()))
-print(f"Исходный список: {lst}")
-new_lst = []
-[new_lst.append(i) for i in lst if i not in new_lst]
-print(f"Список из неповторяющихся элементов: {new_lst}")
+#задача 2 -----------------------------------------------------------------------------
 
 
-#Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
+incorrect_staff = ['инженер-конструктор Игорь', 'главный бухгалтер МАРИНА',
+           'токарь высшего разряда нИКОЛАй', 'директор аэлита']
+
+for correct_staff in incorrect_staff:
+    print(hi.format(correct_staff.split()[-1].title()))
 
 
-import random
+#задача 3 -----------------------------------------------------------------------------
 
 
-def write_file(st):
-    with open('file33.txt', 'w') as data:
-        data.write(st)
+list = [57.8, 46.51, 97, 66, 19.48, 83, 33.11, 55, 7.77, 49]
+
+store_id = id(list)
+print(f"{'a':-^79}")
+end_word: str = ", "
+
+for i, num in enumerate(list):
+
+    fix_price = str(f"{float(num):.2f}").split(".")
+
+    if i == len(list) - 1:
+        end_word = "\n"
+
+    print(f"{fix_price[0]} руб {fix_price[1]} коп", end=end_word)
 
 
-def rnd():
-    return random.randint(0, 101)
+print(f"{'b':-^79}")
+print(f"id перед сортировкой {store_id}")
+list.sort()
+print(list)
+print(f"id после сортировки {id(list)}")
+
+print(f"{'c':-^79}")
+
+copy_of_list = list.copy()
+copy_of_list.sort(reverse=True)
+
+print(copy_of_list)
+print(store_id)
+print(id(copy_of_list))
 
 
-def create_mn(k):
-    lst = [rnd() for i in range(k + 1)]
-    return lst
-
-
-def create_str(sp):
-    lst = sp[::-1]
-    wr = ''
-    if len(lst) < 1:
-        wr = 'x = 0'
-    else:
-        for i in range(len(lst)):
-            if i != len(lst) - 1 and lst[i] != 0 and i != len(lst) - 2:
-                wr += f'{lst[i]}x^{len(lst) - i - 1}'
-                if lst[i + 1] != 0:
-                    wr += ' + '
-            elif i == len(lst) - 2 and lst[i] != 0:
-                wr += f'{lst[i]}x'
-                if lst[i + 1] != 0:
-                    wr += ' + '
-            elif i == len(lst) - 1 and lst[i] != 0:
-                wr += f'{lst[i]} = 0'
-            elif i == len(lst) - 1 and lst[i] == 0:
-                wr += ' = 0'
-    return wr
-
-
-k = int(input("Введите натуральную степень k = "))
-koef = create_mn(k)
-write_file(create_str(koef))
+print(f"{'d':-^79}")
+print("Цены пяти самых дорогих товаров", list[-6:-1])
